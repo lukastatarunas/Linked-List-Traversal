f3cae193f6c03f81944e0c08a7fd481fce74bea5
lukastatarunas <lukasttatarunas@gmail.com>
2019-09-08 13:15:32 +0300

    Added more values to the linked list

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ sll.push(2)
 sll.push(3)
 sll.push(4)
 sll.push(5)
+sll.push(6)
+sll.push(7)
+sll.push(8)
+sll.push(9)
+sll.push(10)
+sll.push(11)
+sll.push(12)
+sll.push(13)
+sll.push(14)
+sll.push(15)
+sll.push(16)
 
 function fifthFromEnd(sll, k) {
     let node = sll.head, i = 1, fifthNode
